f7d592f8011a51f0262554d46e1b1df11dc8bfec
tania.voyat <tetiana.voyat@gmail.com>
2020-09-20 15:53:04 +0300

    Updates

---
+++ precache-manifest.3deec65a5b33fa32e6ab6165159298d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25e89a405e012d103a875bf86e7b4b09",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "dc54ab649ed7706f2630",
+    "url": "/goit-react-hw-01-components/static/css/main.63a51fd8.chunk.css"
+  },
+  {
+    "revision": "13a0aabfa36fdf904150",
+    "url": "/goit-react-hw-01-components/static/js/2.587c3af6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.587c3af6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc54ab649ed7706f2630",
+    "url": "/goit-react-hw-01-components/static/js/main.bbddff85.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
